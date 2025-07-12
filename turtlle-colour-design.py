e1bc1939e82d798e4cb55000c478196870f2c0f5
Authoi234 <jawadkhan.authoi@gmail.com>
2025-07-12 16:20:22 +0600

    unnecessary  modules removed from python colourful design

--- turtlle-colour-design.py
+++ turtlle-colour-design.py
@@ -1,7 +1,4 @@
 import turtle
-import colors
-import random
-import math
 
 turtle.shape("turtle")
 turtle.speed(0)
@@ -10,6 +7,6 @@ turtle.pencolor("red")
 turtle.begin_fill()
 for _ in range(36):  
   turtle.forward(200)
-  turtle.left(170)
+  turtle.left(170) 
 turtle.end_fill()
 turtle.done()
